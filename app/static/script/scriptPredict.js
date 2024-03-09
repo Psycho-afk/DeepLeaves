d57c71db6073b8ad1afeae66be3e1182f6bd64e5
DESKTOP-RA1UK1H\DDA98 <dda9827@hotmail.com>
2024-03-08 20:30:27 -0500

    funcionalidad agregada para utilizar la barra de progreso

--- app/static/script/scriptPredict.js
+++ app/static/script/scriptPredict.js
@@ -1,4 +1,35 @@
+
+var progressBar = document.getElementById('progress-bar')
+var predictButton = document.querySelector('button.btn-success');
+
+function mostrarResultados(results){
+    var resultsDiv = document.getElementById('results');
+    var prediction = results.prediction;
+    var similarLeaves = results.similar_leaves;
+
+    var html = "<p>" + prediction + "</p>";
+
+    if (similarLeaves && Array.isArray(similarLeaves) && similarLeaves.length > 0) {
+        html += "<p>Hojas similares:</p><ul>";
+
+        for (var i = 0; i < similarLeaves.length; i++) {
+            html += "<li>" + similarLeaves[i].class + " (Similitud: " + similarLeaves[i].similarity.toFixed(2) + ")</li>";
+        }
+
+        html += "</ul>";
+    } else {
+        html += "<p>No se encontraron hojas similares.</p>";
+    }
+
+    resultsDiv.innerHTML = html; 
+
+}
+
 function predict() {
+    // oculta la barra de progreso y deshabilita el boton
+    progressBar.style.display = 'block';
+    predictButton.disabled = true;
+
     var form = document.getElementById('upload-form');
     var formData = new FormData(form);
 
@@ -7,33 +38,33 @@ function predict() {
         body: formData
     })
     .then(response => {
+
+        // Establece el ancho de la barra de progreso al 100%
+        progressBar.querySelector('.progress-bar').style.width = '100%';
+
+        // se oculta la barra de progreso
+        progressBar.style.display = 'none';
+
         if (!response.ok) {
             throw new Error('Error en la solicitud: ' + response.statusText);
         }
         return response.json();
     })
     .then(results => {
-        var resultsDiv = document.getElementById('results');
-        var prediction = results.prediction;
-        var similarLeaves = results.similar_leaves;
 
-        var html = "<p>" + prediction + "</p>";
+        // muestra los resultados
+        mostrarResultados(results);
 
-        if (similarLeaves && Array.isArray(similarLeaves) && similarLeaves.length > 0) {
-            html += "<p>Hojas similares:</p><ul>";
-
-            for (var i = 0; i < similarLeaves.length; i++) {
-                html += "<li>" + similarLeaves[i].class + " (Similitud: " + similarLeaves[i].similarity.toFixed(2) + ")</li>";
-            }
-
-            html += "</ul>";
-        } else {
-            html += "<p>No se encontraron hojas similares.</p>";
-        }
-
-        resultsDiv.innerHTML = html;
+        // habilita el boton despues de recibir una respuesta
+        predictButton.disabled = false;
     })
     .catch(error => {
+        // oculta la barra de progreso
+        progressBar.style.display = 'none';
+
+        //habilitar el boton en caso de error
+        predictButton.disabled = false;
+
         console.error("Error:", error);
     });
 }
